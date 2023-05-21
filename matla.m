f800906499d89525b72055951c15e09923b527dd
brookdpg <brookdpg@users.noreply.github.com>
2023-05-21 18:45:40 -0400

    adding scans

--- matla.m
+++ matla.m
@@ -83,3 +83,35 @@ def backtest_options(options):
   function netReturn = calculateNetReturn(data) 
   % Calculate the net return for each period                    netReturn = diff(data)./data(1:end-1); 
   end
+  
+  
+  
+  
+  %%%%%%coiled spring scan
+  
+  % Assuming 'data' is your data table and 'Date' column is the index.
+% The data should have 'Close' and 'Volume' columns. Replace 'your_data_file.csv' with your file
+
+data = readtable('your_data_file.csv'); 
+
+% Calculate RSI70, RSI14, and EMA200
+rsi70 = rsindex(data.Close, 70);
+rsi14 = rsindex(data.Close, 14);
+avgc200 = movavg(data.Close, 'exponential', 200);
+
+% Calculate two days average of RSI70
+rsi70_2 = movavg(rsi70, 'simple', 2);
+
+% calculate two days ago's close
+c2 = [NaN; NaN; data.Close(1:end-2)]; 
+
+% Calculate the price drop more than 4% in the last two trading sessions.
+price_drop = ((c2 - data.Close) ./ c2) * 100 > 4;
+
+% Calculate average volume for the last 50 bars
+avgv50 = movavg(data.Volume, 'simple', 50) > 100000;
+
+% Now do the scan
+scan = price_drop & rsi70_2 > 50 & rsi14 > 50 & data.Close > avgc200 & data.Close > 7 & avgv50 > 1000;
+
+% Now 'scan' is a logical array which is true where your conditions are met
